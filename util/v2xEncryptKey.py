a931cf6328762f1bb46af39d6a84cd1e1378b802
Stephane Viau <stephane.viau@nxp.com>
2020-07-20 16:10:13 +0200

    V2XSW-435 SE: Add key injection tests for DXL EMU C0 MEK
    Signed-off-by: Stephane Viau <stephane.viau@nxp.com>

--- util/v2xEncryptKey.py
+++ util/v2xEncryptKey.py
@@ -67,6 +67,7 @@ def usage():
    print('              KEK Key Encryption Key to be used')
    print('              ' + color.BOLD + '  qxp_common ' + color.END + " i.MX8 QXP Common KEK")
    print('              ' + color.BOLD + '  dxl_common ' + color.END + " i.MX8 DXL Common KEK")
+   print('              ' + color.BOLD + '  dxlphantom_common ' + color.END + " i.MX8 DXL EMU C0 Common KEK")
    print('')
 
 
@@ -164,6 +165,8 @@ def main(argv):
       kek = b'\x10\x2b\xcb\xe5\x4d\xd7\xb2\x33\x94\x6a\xd9\xb0\xa8\x54\x27\xaf\xd5\x16\xf1\x8e\x6e\xa4\xf7\x4b\xb8\x35\x1c\x37\x26\x48\xc7\xfe'
    elif arg_kek == 'dxl_common':
       kek = b'\xda\xec\x80\xc0\x0b\xbb\x02\xba\xc8\x23\x1f\x72\x40\x54\x5c\x5e\xa4\xa8\x1d\xd9\x7d\x66\x68\xf0\x4e\x64\x41\xe1\xb1\x93\x72\x8f'
+   elif arg_kek == 'dxlphantom_common':
+      kek = b'\x07\xa0\xbe\x6a\x9b\xcc\xd9\x54\xcf\xc7\x48\xb5\xa0\xf2\x59\x2c\x7d\x94\xb7\xa6\x1d\xf4\x9a\x8a\xdc\x2f\x53\xc5\xbd\x1d\x34\xc6'
    else:
       print(color.RED + 'ERROR' + color.END + ': Undefined KEK!\n')
       usage();
